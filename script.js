7fdef16b3a1c7744df788f92bcd58155d4f26c8a
DavidAiyede <davidaiyede27@gmail.com>
2023-08-24 21:26:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+var stars = document.getElementsByClassName("fa-star")
+var emoji = document.getElementById("emoji")
+
+const one = () =>{
+    stars[0].style.color = "#ffd93b";
+    stars[1].style.color = "#e4e4e4"
+    stars[2].style.color = "#e4e4e4"
+    stars[3].style.color = "#e4e4e4"
+    stars[4].style.color = "#e4e4e4"
+    emoji.style.transform = "translateX(0)"
+}
+const two = () =>{
+    stars[0].style.color = "#ffd93b";
+    stars[1].style.color = "#ffd93b";
+    stars[2].style.color = "#e4e4e4"
+    stars[3].style.color = "#e4e4e4"
+    stars[4].style.color = "#e4e4e4"
+    emoji.style.transform = "translateX(-100px)"
+}
+const three = () =>{
+    stars[0].style.color = "#ffd93b";
+    stars[1].style.color = "#ffd93b";
+    stars[2].style.color = "#ffd93b";
+    stars[3].style.color = "#e4e4e4"
+    stars[4].style.color = "#e4e4e4"
+    emoji.style.transform = "translateX(-200px)"
+}
+const four = () =>{
+    stars[0].style.color = "#ffd93b";
+    stars[1].style.color = "#ffd93b";
+    stars[2].style.color = "#ffd93b";
+    stars[3].style.color = "#ffd93b";
+    stars[4].style.color = "#e4e4e4"
+    emoji.style.transform = "translateX(-300px)"
+}
+const five = () =>{
+    stars[0].style.color = "#ffd93b";
+    stars[1].style.color = "#ffd93b";
+    stars[2].style.color = "#ffd93b";
+    stars[3].style.color = "#ffd93b";
+    stars[4].style.color = "#ffd93b";
+    emoji.style.transform = "translateX(-400px)"
+}
